c73b781f3a9697189c40070178668780abc60476
zymfe <1047832475@qq.com>
2019-10-18 23:39:24 +0800

    rename dirs and add oop

---
+++ iOS/Objective-C/oop/oop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oop
+//
+//  Created by 赵一鸣 on 2019/10/18.
+//  Copyright © 2019 赵一鸣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
