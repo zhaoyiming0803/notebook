b9c7d60c471d43cd1fb79ca70434c5e8eb989ffe
zymfe <1047832475@qq.com>
2019-10-22 18:41:17 +0800

    update: buffer

--- Node/buffer/index.js
+++ Node/buffer/index.js
@@ -20,3 +20,18 @@ console.log(Buffer.isBuffer([1]));
 
 console.log(Buffer.isEncoding('utf-8'));
 console.log(Buffer.isEncoding('xxx'));
+
+console.log('----------');
+const buf4 = Buffer.alloc(5);
+console.log(buf4);
+buf4.writeInt32BE(Buffer.from('12345'), 0);
+console.log(buf4);
+
+console.log('------------');
+
+const buf5 = Buffer.alloc(8);
+buf5.writeInt32BE(0x01020304, 0);
+console.log(buf5);
+buf5.writeInt32LE(0x05060708, 4);
+console.log(buf5);
+console.log(buf5.toString('utf8'));
