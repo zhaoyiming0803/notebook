b1759389550bf080f3fa2dc6a300d2c48715fa98
zymfe <1047832475@qq.com>
2019-10-23 16:21:44 +0800

    test: upgrade duplex

--- Node/net/upgraded-duplex/client.js
+++ Node/net/upgraded-duplex/client.js
@@ -2,9 +2,27 @@ const net = require('net');
 const { ids } = require('../config');
 
 const client = new net.Socket();
+let remnat = null;
 
 client.on('data', buf => {
-  
+  // console.log(buf.readInt16BE(0));
+  // console.log(buf.readInt32BE(2));
+  // console.log(buf.readInt32BE(6));
+  if (remnat) {
+    buf = Buffer.concat([remnat, buf]);
+  }
+
+  let packageLength = 0;
+
+  while (packageLength = isComplete(buf)) {
+    const package = buf.slice(0, packageLength);
+    buf = buf.slice(packageLength);
+    remnat = null;
+    const content = decode(package);
+    console.log(content);
+  }
+
+  remnat = buf;
 });
 
 client.connect({
@@ -12,4 +30,36 @@ client.connect({
   port: 3000
 });
 
-client.write(Buffer.from('hello'));
+let seq = 0;
+
+function encode (id) {
+  const body = Buffer.alloc(4);
+  body.writeInt32BE(id);
+
+  const header = Buffer.alloc(6);
+  header.writeInt16BE(seq++);
+  header.writeInt32BE(body.length, 2);
+
+  return Buffer.concat([header, body]);
+}
+
+function decode (buf) {
+  const header = buf.slice(0, 6);
+  const body = buf.slice(6);
+  return {
+    seq: header.readInt16BE(),
+    bodyLength: header.readInt32BE(2),
+    body: body.toString()
+  }
+}
+
+function isComplete (buf) {
+  if (buf.length < 6) {
+    return 0;
+  }
+  return 6 + buf.readInt32BE(2);
+}
+
+ids.forEach(id => {
+  client.write(encode(id));
+});
--- Node/net/upgraded-duplex/server.js
+++ Node/net/upgraded-duplex/server.js
@@ -2,9 +2,55 @@ const net = require('net');
 const { userMap } = require('../config');
 
 const server = net.createServer(socket => {
+  let remnat = null;
+
   socket.on('data', buf => {
-    
+    let packageLength = 0;
+
+    if (remnat) {
+      buf = Buffer.concat([remnat, buf]);
+    }
+
+    while (packageLength = isComplete(buf)) {
+      const package = buf.slice(0, packageLength);
+      buf = buf.slice(packageLength);
+      remnat = null;
+      const content = decode(package);
+      console.log(content);
+      setTimeout(() => {
+        socket.write(encode(content));
+      }, Math.random() * 2000);
+    }
+
+    remnat = buf;
   });
 });
+
+function decode (buf) {
+  const header = buf.slice(0, 6);
+  const seq = header.readInt16BE(0);
+  const body = buf.slice(6).readInt32BE();
+  return {
+    seq,
+    data: body
+  }
+}
+
+function encode (content) {
+  const body = Buffer.from(userMap[content.data]);
+
+  const header = Buffer.alloc(6);
+  header.writeInt16BE(content.seq);
+  header.writeInt32BE(body.length, 2);
+
+  return Buffer.concat([header, body]);
+}
+
+function isComplete (buf) {
+  if (buf.length < 6) {
+    return 0;
+  }
+  return 6 + buf.readInt32BE(2); // header 的长度 + body 的长度
+}
 
 server.listen(3000);
