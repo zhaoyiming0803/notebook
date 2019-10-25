8e98ca08b0620b4c4aca4d2632066e872c723dde
zymfe <1047832475@qq.com>
2019-10-25 19:41:05 +0800

    test: ssr template engine

---
+++ Node.js/ssr/index.js
@@ -0,0 +1,30 @@
+const vm = require('vm');
+
+const article = {
+  title: 'article title',
+  content: 'article content <script>alert("danger...");</script> ...'
+};
+
+const template = '`<h1>${article.title}</h1><div>${xss(article.content)}</div>`';
+
+const htmlContent = vm.runInNewContext(template, {
+  article,
+
+  helper (str) {
+    // ...
+    return str;
+  },
+  
+  xss (str) {
+    if (!str) return '';
+    return str.toString()
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&quot;');
+  }
+})
+
+console.log(htmlContent);
