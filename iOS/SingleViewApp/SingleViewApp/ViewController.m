248c052bc6c01fc7a16944bf7908fa9d9a553585
zymfe <1047832475@qq.com>
2019-11-08 23:21:51 +0800

    test: UITableViewDelegate

--- iOS/SingleViewApp/SingleViewApp/ViewController.m
+++ iOS/SingleViewApp/SingleViewApp/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UITableViewDataSource>
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @end
 
@@ -20,6 +20,7 @@
     
     UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
     tableView.dataSource = self;
+    tableView.delegate = self;
     [self.view addSubview:tableView];
 }
 
@@ -29,7 +30,6 @@
 
 // 每个 cell 进入视图，都要执行当前函数
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"row: %ld", indexPath.row);
     // UITableViewCell 的复用机制：先去系统回收池获取 cell
     // 如果能取到，则直接复用
     // 如果没有可用的 cell，则再次创建
@@ -37,12 +37,23 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
     }
-    cell.textLabel.text = @"主标题";
+    cell.textLabel.text = [NSString stringWithFormat:@"主标题 - section - %ld", indexPath.section];
     cell.textLabel.textColor = [UIColor redColor];
-    cell.detailTextLabel.text = @"副标题";
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"副标题 - row - %ld", indexPath.row];
     cell.detailTextLabel.textColor = [UIColor blueColor];
     cell.imageView.image = [UIImage imageNamed:@"./icon.bundle/red-packet@3x.png"];
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIViewController *controller = [[UIViewController alloc] init];
+    controller.title = [NSString stringWithFormat:@"%ld", indexPath.row];
+    NSLog(@"%@", controller.title);
+    [self.navigationController pushViewController:controller animated:YES];
 }
 
 @end
---
+++ iOS/UITableViewDelegate.md
@@ -0,0 +1,8 @@
+### UITableViewDelegate
+
+ - 提供滚动过程中，UITableViewCell 的出现、消失时机
+
+ - 提供 UITableViewCell 的高度、headers 以及 footers 的设置
+
+ - 提供 UITableViewCell 各种行为的回调（点击、删除等）
+
