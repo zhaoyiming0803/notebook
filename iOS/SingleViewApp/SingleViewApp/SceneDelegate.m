362d6c7b3664ac77a568a497d85730c53f30365a
zymfe <1047832475@qq.com>
2019-11-10 21:44:41 +0800

    chore: first format codes with XCFormat

--- iOS/SingleViewApp/SingleViewApp/SceneDelegate.m
+++ iOS/SingleViewApp/SingleViewApp/SceneDelegate.m
@@ -10,29 +10,28 @@
 
 @implementation SceneDelegate
 
-
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
+
     YMTableViewController *tab1 = [[YMTableViewController alloc] init];
-    
+
     YMCollectionViewController *tab2 = [[YMCollectionViewController alloc] init];
-    
+
     YMScrollViewController *tab3 = [[YMScrollViewController alloc] init];
-    
+
     YMNormalViewController *tab4 = [[YMNormalViewController alloc] init];
-    
-    UITabBarController * tabbarController = [[UITabBarController alloc] init];
+
+    UITabBarController *tabbarController = [[UITabBarController alloc] init];
     [tabbarController setViewControllers:@[tab1, tab2, tab3, tab4]];
-    
+
     tabbarController.delegate = self;
-    
+
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:tabbarController];
-    
+
     self.window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
-    [self.window setRootViewController: navigationController];
+    [self.window setRootViewController:navigationController];
     [self.window makeKeyAndVisible];
 }
 
@@ -49,30 +48,25 @@
     // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
 }
 
-
 - (void)sceneDidBecomeActive:(UIScene *)scene {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 }
-
 
 - (void)sceneWillResignActive:(UIScene *)scene {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
 }
 
-
 - (void)sceneWillEnterForeground:(UIScene *)scene {
     // Called as the scene transitions from the background to the foreground.
     // Use this method to undo the changes made on entering the background.
 }
-
 
 - (void)sceneDidEnterBackground:(UIScene *)scene {
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state information
     // to restore the scene back to its current state.
 }
-
 
 @end
